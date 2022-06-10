55518750655e5a3e48f471aa76347e4d791fa28d
yumauda <firststep.yumauda@gmail.com>
2022-06-10 14:59:44 +0900

    2022.06.10 15:00 水玉ドット調整・ハート出力

--- home.php
+++ home.php
@@ -14,7 +14,7 @@
                     <?php the_post(); ?>
                     <div class="p-home__row">
                         <div class="p-home__title">
-                            <p class="p-home__date"><?php echo get_the_time('Y/n/j') ?></p>
+                            <p class="p-home__date"><?php echo get_the_time('Y.n.j') ?></p>
                         </div>
                         <div class="p-home__description">
                             <p class="p-home__text"><?php echo wp_trim_words(get_the_title(), 60, '...'); ?></p>
